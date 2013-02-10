31c57bc922921b5e0e39b1243e4e23997f94daa1
Jan Grassegger <jan@anycook.de>
2013-02-10 21:54:35 +0100

    added data

---
+++ src/jquery.appendgithub.js
@@ -0,0 +1,41 @@
+/*!
+ * jQuery appendGitHub Plugin v0.1
+ * http://anycook.de
+ *
+ * Copyright (c) 2011, 2013 Jan Grassegger
+ * Released under the MIT license.
+ * http://jquery.org/license
+ */
+
+(function ( $, window, document, undefined ){
+	
+	$.fn.appendGitHub = function(path, options){		
+		
+		return this.each(function(){
+			var $this = $(this);
+      	
+	      	var settings = {
+	      		domain : "https://api.github.com/repos/",
+	      		parseMarkdown : false
+	      	}
+	      	
+	      	$.extend(settings, options);
+	      	
+	      	$.ajax({
+				url:settings.domain+path,
+				type:"GET",
+				data:{callback:"?"},
+				dataType:"jsonp",
+				success:function(json){
+					var content = Base64.decode(json.data.content);
+					
+					if(settings.parseMarkdown)
+						content = marked(content);
+					// var content = window.atob(json.data.content);
+					$this.append(content);
+				}
+			});
+		});
+      	
+	}
+})( jQuery, window, document );
